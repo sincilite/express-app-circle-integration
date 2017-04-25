5c874f5430cb1b23c10cd134e98ad05b98fea6c8
Mike van Rooyen <mike@zaxity.com>
2017-04-25 13:14:07 +0100

    Fix style JS errors

--- config/components/development.js
+++ config/components/development.js
@@ -1,16 +1,18 @@
-"use strict";
+(function() {
+    "use strict";
 
-const mysql = require("mysql");
+    const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host     : '172.17.0.2',
-    user     : 'root',
-    password : 'test',
-    database : 'teamster'
-});
+    var connection = mysql.createConnection({
+        host: "172.17.0.2",
+        user: "root",
+        password: "test",
+        database: "teamster"
+    });
 
-const config = {
-    db: connection
-}
+    const config = {
+        db: connection
+    };
 
-module.exports = config;
+    module.exports = config;
+}());
--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
-"use strict";
+(function() {
+    "use strict";
+    console.log(process.env);
+    const common = require("./components/common");
+    const environment = require("./components/" + process.env.APP_ENV);
 
-const common = require("./components/common");
-const environment = require("./components/"+process.env.APP_ENV);
-
-module.exports = Object.assign({}, common, environment);
+    module.exports = Object.assign({}, common, environment);
+}());
