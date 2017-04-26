b83cb07fe6e2c4f025c48d6970429681a834aab3
Mike van Rooyen <mike@zaxity.com>
2017-04-26 16:03:43 +0100

    Fix code style

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@
     "use strict";
     console.log(process.env);
     const common = require("./components/common");
+
     //const environment = require("./components/" + process.env.APP_ENV);
 
     //module.exports = Object.assign({}, common, environment);
