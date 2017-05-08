1b6a2ec0472f83d10fd85c2cb86c2689b885cfa6
Mike van Rooyen <mike@zaxity.com>
2017-05-08 13:26:17 +0100

    Dynamically add IP address to security group

--- whitelist_ip.sh
+++ whitelist_ip.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo "this computers public ip address is $public_ip_address"
-aws ec2 help
-#aws ec2 authorize-security-group-ingress --region <YOUR_REGION> --group-id <YOUR_SECURITY_GROUP_ID> --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": <YOUR_PORT_YOU_NEED_OPEN>, \"ToPort\": <YOUR_PORT_YOU_NEED_OPEN>, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
+aws ec2 authorize-security-group-ingress --region eu-west-1 --group-id sg-974520ee --protocol tcp --port 22 --cidr $public_ip_address/32
