cfb7b3c5e12aca5ece93853a16fb91f6f45776ab
Mike van Rooyen <mike@zaxity.com>
2017-05-08 14:17:40 +0100

    Add security group to environment vars

--- remove_ip.sh
+++ remove_ip.sh
@@ -2,4 +2,4 @@
 
 public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo "Removing $public_ip_address"
-aws ec2 revoke-security-group-ingress --region eu-west-1 --group-id sg-974520ee --protocol tcp --port 22 --cidr $public_ip_address/32
+aws ec2 revoke-security-group-ingress --region eu-west-1 --group-id $security_group_id --protocol tcp --port 22 --cidr $public_ip_address/32
--- whitelist_ip.sh
+++ whitelist_ip.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo "Authorising $public_ip_address"
-aws ec2 authorize-security-group-ingress --region eu-west-1 --group-id sg-974520ee --protocol tcp --port 22 --cidr $public_ip_address/32
+aws ec2 authorize-security-group-ingress --region eu-west-1 --group-id $security_group_id --protocol tcp --port 22 --cidr $public_ip_address/32
