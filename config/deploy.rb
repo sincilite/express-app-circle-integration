d2d75a76b0b567bb58056a756a4bd356339bcd32
Mike van Rooyen <mike@zaxity.com>
2017-05-08 16:02:27 +0100

    Refactor linked folders

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,10 +21,10 @@ set :deploy_to, "/data/someapplication.com"
 # set :pty, true
 
 # Default value for :linked_files is []
-#append :linked_files, "config/database.yml", "config/secrets.yml"
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "logs", "uploads", "uploads/images", "uploads/documents"
+append :linked_dirs, "logs", "uploads/images", "uploads/documents"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
